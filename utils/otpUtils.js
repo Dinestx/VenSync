0b2d14e3e7fd0ba4a6fca201023b5c11813e6ac8
Abhishek <meaviishek@gmail.com>
2025-01-08 16:38:29 +0530

    first commit

--- utils/otpUtils.js
+++ utils/otpUtils.js
@@ -1,6 +1,6 @@
 import twilio from 'twilio'
 
-const accountSid = process.env.ACCOUNT_SID;
+const accountSid =process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
