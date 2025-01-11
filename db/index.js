b858b5da6767c5a1ed1b7986e28223b966b801e0
Abhishek <meaviishek@gmail.com>
2025-01-11 06:03:21 +0530

    j

--- db/index.js
+++ db/index.js
@@ -6,6 +6,8 @@ dotenv.config();  // To load environment variables from a .env file
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('MongoDB connected successfully');
   } catch (error) {
