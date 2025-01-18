d2143321ce6f7ea7c8abbb947958378a42e58c31
Abhishek <meaviishek@gmail.com>
2025-01-18 16:05:05 +0530

    new backend changes

--- models/complaints.js
+++ models/complaints.js
@@ -1,7 +1,12 @@
 // models/Complaint.js
 import mongoose from "mongoose";
-
+import crypto from "crypto";
 const complaintSchema = new mongoose.Schema({
+  complaint_id: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
@@ -38,7 +43,13 @@ const complaintSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-
+complaintSchema.pre("save", async function (next) {
+  if (!this.complaint_id) {
+    // Generate a unique 9-character alphanumeric ID
+    this.complaint_id = crypto.randomBytes(5).toString("hex").slice(0, 9).toUpperCase();
+  }
+  next();
+});
 const Complaint = mongoose.model('Complaint', complaintSchema);
 
 export default Complaint
